d2f5c6fc9ae9036f010bdbc9b84f4d2d242c3dbd
Mijael Kuma <mijaelkuma@gmail.com>
2025-06-04 01:30:43 +0000

    I set up firebase storage it is empty but it is there

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
